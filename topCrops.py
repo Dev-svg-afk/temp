afd0bdaea933f4931dca1157bdf48d7033a38252
Dev-svg-afk <24f2003048@ds.study.iitm.ac.in>
2025-09-16 23:03:42 +0530

    Update topCrops.py

--- topCrops.py
+++ topCrops.py
@@ -1,14 +1,23 @@
 import requests
-def getPrices(state="Karnataka",crops=["Rice,Wheat"]):
-  resp = requests.get(
-      f"https://api.data.gov.in//resource/9ef84268-d588-465a-a308-a864a43d0070",
-      params={
-          "api-key": API_KEY,
-          "format": "json",
-          "filters[state.keyword]" : state,
-          "filters[commodity]" : crops,
-          "limit": 3,
-      }
-  )
-  data = resp.json()
-  return data.get("records", [])
+def getPrices(state="Karnataka",crops=["water melon"]):
+  nameMap = {"soyabeans":"Soyabean","cowpeas":"cowpea","groundnuts":"groundnut","watermelon":"water melon"}
+  crops = [nameMap[x] if x in nameMap else x for x in crops]
+  tops = []
+  for crop in crops:
+    resp = requests.get(
+        f"https://api.data.gov.in//resource/9ef84268-d588-465a-a308-a864a43d0070",
+        params={
+            "api-key": API_KEY,
+            "format": "json",
+            "filters[state.keyword]" : state,
+            "filters[commodity]" : crop,
+            "limit": 1,
+        }
+    )
+    data = resp.json()
+    records = data.get("records", [])
+    if records:
+      x = records[0]
+      tops.append([x['commodity'],x['min_price'],x['max_price'],x['modal_price']])
+  tops.sort(key=lambda x: int(x[-2]), reverse=True)
+  return tops[:3]
